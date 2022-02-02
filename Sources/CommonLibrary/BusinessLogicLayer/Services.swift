30e15d4b3a0494624ef2763cba7f93267cee5a1e
Sergey Shupegin <shupeginsergei@gmail.com>
2022-02-02 09:50:14 +0300

    Update Services

---
+++ Sources/CommonLibrary/BusinessLogicLayer/Services.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Shupegin Sergey on 02.02.2022.
+//
+
+import Foundation
+
+class Services {
+    
+    static let shared = Services()
+    private init() {
+    }
+    
+}
