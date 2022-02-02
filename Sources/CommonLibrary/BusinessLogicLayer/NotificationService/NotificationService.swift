7c4114714958a90a9ea4597de6595928ec1747ea
Sergey Shupegin <shupeginsergei@gmail.com>
2022-02-02 12:53:15 +0300

    Update

---
+++ Sources/CommonLibrary/BusinessLogicLayer/NotificationService/NotificationService.swift
@@ -0,0 +1,41 @@
+//
+//  NotificationService.swift
+//  LanguageCards
+//
+//  Created by Shupegin Sergey on 01.12.2021.
+//
+
+import Foundation
+
+open class NotificationService: NotificationServicePublisher {
+    
+    // MARK: - Singlton
+    
+    static public let shared = NotificationService()
+    private init () {}
+    
+    // MARK: - Properties
+    
+    private var listeners = [NotificationServiceListener]()
+    
+    // MARK: - NotificationServicePublisher
+    
+    public func addListener(_ listener: NotificationServiceListener) {
+        if listeners.filter({ $0.isEqual(to: listener) }).isEmpty {
+            listeners.append(listener)
+        }
+    }
+    
+    public func removeListener(_ listener: NotificationServiceListener) {
+        
+        for i in 0..<listeners.count {
+            if listeners[i].isEqual(to: listener) {
+                listeners.remove(at: i)
+                return
+            }
+        }
+    }
+    
+    // MARK: - NotificationServiceProtocol
+    
+}
